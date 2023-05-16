c66da380009cfd9e63656af0b31303ad99f9c500
Sophia Wang <huiwang@redhat.com>
2023-05-16 03:37:39 +0000

    Use render function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,20 +2,22 @@
 
 # Create your views here.
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     #output = ", ".join([q.question_text for q in latest_question_list])
-    template = loader.get_template("polls/index.html")
+    #template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
     #return HttpResponse("Hello, world. You're at the polls index.")
     #return HttpResponse(output)
-    return HttpResponse(template.render(context, request))
+    #return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)    
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s" % question_id)
