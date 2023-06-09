03cda267c48652416de2635f0a05cb8fccac4e72
Sophia Wang <huiwang@redhat.com>
2023-06-09 05:58:07 +0000

    customise the admin change list

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -11,10 +11,10 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-            (None, {"fields": ["question_text"]}),
-            ("Date information", {"fields": ["pub_date"]}),
-    ]
+    list_display = ["question_text", "pub_date",
+            "was_published_recently"]
+    list_filter = ['pub_date']
+    search_fields = ["question_text"]
     inlines = [ChoiceInline]
 
 
